6678a6021721f8009e782f561739190bd81d9f08
Pilo <yassel.piloto@BOS-84LT433.duckcreek.com>
2023-04-07 14:36:50 -0400

    add caffeinate to hs

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -78,3 +78,20 @@ hs.hotkey.bind({"cmd", "alt"}, "A", function()
   audioChooser:show()
 end)
 
+
+-- caffeinate
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "I", function()
+  hs.caffeinate.set("displayIdle", true, true)
+  hs.caffeinate.set("systemIdle", true, true)
+  hs.caffeinate.set("system", true, true)
+  hs.alert.show("Preventing Sleep")
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "O", function()
+  hs.caffeinate.set("displayIdle", false, true)
+  hs.caffeinate.set("systemIdle", false, true)
+  hs.caffeinate.set("system", false, true)
+  hs.alert.show("Allowing Sleep")
+end)
+
