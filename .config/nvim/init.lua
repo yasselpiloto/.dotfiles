aab3506f81b8741ef49fbee2f1b12a7b8f863827
Pilo <yassel.piloto@duckcreek.com>
2023-05-19 14:00:43 -0400

    update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,4 +8,13 @@ vim.opt.relativenumber = false
 
 vim.g.mapleader = ','
 
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 require('plugins')
+
+require('nvim-tree').setup {}
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,24 @@
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+-- then run :PackerInstall
+
+vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     use 'preservim/nerdcommenter'
+
+    use {
+      'nvim-tree/nvim-tree.lua',
+      requires = {
+        'nvim-tree/nvim-web-devicons', -- optional
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end
+    }
+
 end)
+
+
